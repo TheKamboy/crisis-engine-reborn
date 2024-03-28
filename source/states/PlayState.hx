bd78ffb4da1bea890eefcc0e32603adf9f075e70
TheKamboy <yaluigi10@gmail.com>
2024-03-27 17:05:27 -0700

    part 1

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -497,7 +497,7 @@ class PlayState extends MusicBeatState
 		uiGroup.add(timeBar);
 		uiGroup.add(timeTxt);
 
-		healthBar = new Bar(0, FlxG.height * (!ClientPrefs.data.downScroll ? 0.89 : 0.11), 'healthBar', function() return health, 0, 2);
+		healthBar = new Bar(0, FlxG.height * (!ClientPrefs.data.downScroll ? 0.89 : 0.11), 'healthBarTransparent', function() return health, 0, 2);
 		healthBar.screenCenter(X);
 		healthBar.leftToRight = false;
 		healthBar.scrollFactor.set();
